c580fd5bf1e6458326a1a43493e1ff1111ede088
pratik-thakur <pratikthakur421@gmail.com>
2021-11-07 15:51:13 +0530

    used salesorder route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(cors());
 const authenticateAdminToken = require('./Middlewares/authenticateAdminToken');
 const getToken = require('./routeFunctions/getToken/getToken');
 const Admin = require('./routeFunctions/Admin');
+const salesOrder = require('./routeFunctions/salesorder/salesorder');
 /////////////////////Routes///////////////////////////////////
 //login route
 app.get('/',(req,res)=>{
@@ -15,6 +16,7 @@ app.post('/login',getToken);
 //admin routes (only authenticated admin can fetch this)
 app.post('/createuser',authenticateAdminToken, Admin.createUser);
 
+app.use(salesOrder);
 
 app.all('*', (req, res) => {
 	res.sendStatus(404);
