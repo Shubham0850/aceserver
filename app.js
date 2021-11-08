167aea99b730f12d88ade47ce011ec166267a247
pratik-thakur <pratikthakur421@gmail.com>
2021-11-08 14:08:11 +0530

    made code working after debojeet refactor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cors());
 const authenticateAdminToken = require('./Middlewares/authenticateAdminToken');
 const getToken = require('./routeFunctions/getToken/getToken');
 const Admin = require('./routeFunctions/Admin');
-const salesOrder = require('./routeFunctions/salesorder/salesorder');
+const {createSalesOrder , getSalesorder} = require('./routeFunctions/salesorder/salesorder');
 /////////////////////Routes///////////////////////////////////
 //login route
 app.get('/',(req,res)=>{
@@ -16,7 +16,8 @@ app.post('/login',getToken);
 //admin routes (only authenticated admin can fetch this)
 app.post('/createuser',authenticateAdminToken, Admin.createUser);
 
-app.use(salesOrder);
+app.post('/createsalesorder',createSalesOrder);
+app.get('/getsalesorder',getSalesorder);
 
 app.all('*', (req, res) => {
 	res.sendStatus(404);
