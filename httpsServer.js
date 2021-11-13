54bd9b4c1308cf4970d06db7dcf9b46ba21385ee
Debojeet Jha <debojeetjha@gmail.com>
2021-11-13 13:05:58 +0530

    remove app event listener

--- httpsServer.js
+++ httpsServer.js
@@ -4,10 +4,7 @@ const app = require('./app.js');
 const fs = require('fs');
 const https = require('https');
 
-const PORT = 4000;
-app.listen(PORT, () => {
-	console.log(`App running on port ${PORT}...`);
-});
+const PORT = 443;
 const options = {
 	key: fs.readFileSync('client-key.pem'),
 	cert: fs.readFileSync('client-cert.pem')
