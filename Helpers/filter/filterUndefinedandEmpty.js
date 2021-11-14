7512e03445b8fe38007825bd0e57a403d38850c7
Debojeet Jha <debojeetjha@gmail.com>
2021-11-15 00:17:52 +0530

    update filter

--- Helpers/filter/filterUndefinedandEmpty.js
+++ Helpers/filter/filterUndefinedandEmpty.js
@@ -1,7 +1,7 @@
 const filterUndefinedandEmpty = (obj)=>{
 	var filteredObj = {};
 	for(const key in obj){
-		if(obj[key]!==undefined && obj[key]!==''){
+		if(obj[key]!==undefined && obj[key]!==null){
 			filteredObj[key] = obj[key];
 		}
 	}
